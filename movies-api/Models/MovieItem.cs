e01e105e7981fdffb1b3b6e4420997780696e78e
Jordan <jche681@aucklanduni.ac.nz>
2018-11-21 17:56:55 +1300

    Add project files.

---
+++ movies-api/Models/MovieItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Models
+{
+    public class MovieItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Plot { get; set; }
+        public string Language { get; set; }
+        public string Country { get; set; }
+        public string PosterLink { get; set; }
+        public string ImdbRating { get; set; }
+        public string DateCreated { get; set; }
+    }
+}
