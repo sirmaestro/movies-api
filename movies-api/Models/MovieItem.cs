c900d36a40fe8506c873db5fe09e23791bf20821
Jordan <jche681@aucklanduni.ac.nz>
2018-11-23 17:55:26 +1300

    Changed movie model

--- movies-api/Models/MovieItem.cs
+++ movies-api/Models/MovieItem.cs
@@ -10,14 +10,8 @@ namespace MoviesApi.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Year { get; set; }
-        public string Runtime { get; set; }
         public string Genre { get; set; }
-        public string Director { get; set; }
-        public string Plot { get; set; }
-        public string Language { get; set; }
-        public string Country { get; set; }
         public string PosterLink { get; set; }
-        public string ImdbRating { get; set; }
         public string DateCreated { get; set; }
     }
 }
--- movies-api/Models/SeedData.cs
+++ movies-api/Models/SeedData.cs
@@ -25,14 +25,8 @@ namespace MoviesApi.Models
                     {
                         Title = "Avengers: Infinity War",
                         Year = "2018",
-                        Runtime = "149 min",
                         Genre = "Action, Adventure, Fantasy, Sci-Fi",
-                        Director = "Anthony Russo, Joe Russo",
-                        Plot = "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
-                        Language = "English",
-                        Country = "USA",
-                        PosterLink = "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-                        ImdbRating = "8.6"
+                        PosterLink = "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
                     }
                 );
                 context.SaveChanges();
